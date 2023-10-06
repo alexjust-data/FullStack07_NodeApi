137378196b339f431b32acce5062ee791409ecd6
Alex Just <alexjustdata@gmail.com>
2023-10-06 13:14:34 +0200

    fead: filters searchs

--- nodepop/routes/index.js
+++ nodepop/routes/index.js
@@ -1,64 +1,64 @@
-const express = require('express');
-const router = express.Router();
-const Ad = require('../models/Ad');
+const express = require('express')
+const router = express.Router()
+const Ad = require('../models/Ad')
 
 router.get('/', async (req, res, next) => {
   try {
-    const filterByName = req.query.name;
-    const skip = req.query.skip;
-    const limit = req.query.limit;
-    const sort = req.query.sort;
-    const fields = req.query.fields;
-    const filterByTags = req.query.tags;
-    const filterByType = req.query.venta;
-    const filterByPrice = req.query.price;
+    const filterByName = req.query.name
+    const skip = req.query.skip
+    const limit = req.query.limit
+    const sort = req.query.sort
+    const fields = req.query.fields
+    const filterByTags = req.query.tags
+    const filterByType = req.query.venta
+    const filterByPrice = req.query.price
 
-    const filter = {};
+    const filter = {}
 
     if (filterByTags) {
-      filter.tags = { $in: filterByTags.split(',') };
+      filter.tags = { $in: filterByTags.split(',') }
     }
 
     if (filterByType !== undefined) {
-      filter.venta = filterByType === 'true';
+      filter.venta = filterByType === 'true'
     }
 
     if (filterByName) {
-      filter.name = new RegExp('^' + filterByName, 'i');
+      filter.name = new RegExp('^' + filterByName, 'i')
     }
 
     if (filterByPrice) {
-      const priceRange = filterByPrice.split('-');
+      const priceRange = filterByPrice.split('-')
 
       if (priceRange.length === 2) {
-        const [minPrice, maxPrice] = priceRange.map(parseFloat);
+        const [minPrice, maxPrice] = priceRange.map(parseFloat)
         if (!isNaN(minPrice) && !isNaN(maxPrice)) {
-          filter.price = { $gte: minPrice, $lte: maxPrice };
+          filter.price = { $gte: minPrice, $lte: maxPrice }
         } else if (!isNaN(minPrice)) {
-          filter.price = { $gte: minPrice };
+          filter.price = { $gte: minPrice }
         } else if (!isNaN(maxPrice)) {
-          filter.price = { $lte: maxPrice };
+          filter.price = { $lte: maxPrice }
         }
       } else if (priceRange.length === 1) {
-        const singlePrice = parseFloat(priceRange[0]);
+        const singlePrice = parseFloat(priceRange[0])
         if (!isNaN(singlePrice)) {
-          filter.price = singlePrice;
+          filter.price = singlePrice
         }
       }
     }
 
-    const ads = await Ad.lista(filter, skip, limit, sort, fields);
+    const ads = await Ad.lista(filter, skip, limit, sort, fields)
 
     if (req.originalUrl.startsWith('/api/')) {
       // If the request comes from the API, respond with JSON
-      res.json({ results: ads });
+      res.json({ results: ads })
     } else {
       // If the request comes from the management interface, render the view
-      res.render('index', { ads });
+      res.render('index', { ads })
     }
   } catch (err) {
-    next(err);
+    next(err)
   }
-});
+})
 
-module.exports = router;
+module.exports = router
