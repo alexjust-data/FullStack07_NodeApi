23e7b7df8cdd316a6a203f2354a1813dc12562c9
Alex Just <alexjustdata@gmail.com>
2023-10-06 13:05:51 +0200

    fix: eslint sintax

--- nodepop/lib/basicAuthMiddleware.js
+++ nodepop/lib/basicAuthMiddleware.js
@@ -1,24 +1,23 @@
 /**
  * This file is to protect sensitive data
- * 
+ *
  * $ nodeapp
  * $ npm install basic-auth
  */
 
-const auth = require('basic-auth');
-
+const auth = require('basic-auth')
 
 module.exports = (req, res, next) => {
-    // now it will remove that base64 from the req and transform it 
-    // into an object that has name and pass (it comes in the auth doc)
-    const user = auth(req);
+// now it will remove that base64 from the req and transform it
+// into an object that has name and pass (it comes in the auth doc)
+  const user = auth(req)
 
-    if (!user || user.name !== 'admin' || user.pass !== '1234') {
-        res.set('WWW-Authenticate', 'Basic realm=Authorization required');
-        //res.redirect('/'); // if not authenticated, it redirects back to the root
-        res.sendStatus(401); // if not authenticated, it prompts for credentials
-        return 
-      }
-    
-      next();
+  if (!user || user.name !== 'admin' || user.pass !== '1234') {
+    res.set('WWW-Authenticate', 'Basic realm=Authorization required')
+    // res.redirect('/'); // if not authenticated, it redirects back to the root
+    res.sendStatus(401) // if not authenticated, it prompts for credentials
+    return
+  }
+
+  next()
 }
