f813f725986435f37e82cc5b04317cddd0ba3844
Alex Just <alexjustdata@gmail.com>
2023-10-06 13:03:11 +0200

    fix: eslint sintax

--- nodepop/app.js
+++ nodepop/app.js
@@ -1,59 +1,55 @@
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
-const basicAuthMiddleware = require('./lib/basicAuthMiddleware');
-const swaggerMiddleware = require('./lib/swaggerMiddleware');
+const createError = require('http-errors')
+const express = require('express')
+const path = require('path')
+const cookieParser = require('cookie-parser')
+const logger = require('morgan')
+const basicAuthMiddleware = require('./lib/basicAuthMiddleware')
+const swaggerMiddleware = require('./lib/swaggerMiddleware')
 
-require('./lib/connectMongoose');
+require('./lib/connectMongoose')
 
-
-var app = express();
-
+const app = express()
 
 // view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'))
+app.set('view engine', 'ejs')
 
-app.use(logger('dev'));
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(logger('dev'))
+app.use(express.json())
+app.use(express.urlencoded({ extended: false }))
+app.use(cookieParser())
+app.use(express.static(path.join(__dirname, 'public')))
 
-
-app.use('/', require('./routes/index'));
-app.use('/users', require('./routes/users'));
-app.use('/admin', basicAuthMiddleware, require('./routes/admin')); // protected
+app.use('/', require('./routes/index'))
+app.use('/users', require('./routes/users'))
+app.use('/admin', basicAuthMiddleware, require('./routes/admin')) // protected
 
 // Serves static files from the 'public' folder
-// app.use(express.static('public/images'));
+// app.use(express.static('public/images'))
 
-
-const Ad = require('./models/Ad');
+const Ad = require('./models/Ad')
 
 /**
  * API
  */
-app.use('/api-doc', swaggerMiddleware);
-app.use('/api/ads', require('./routes/api/ads'));
+app.use('/api-doc', swaggerMiddleware)
+app.use('/api/ads', require('./routes/api/ads'))
 
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  next(createError(404));
-});
+app.use(function (req, res, next) {
+  next(createError(404))
+})
 
 // error handler
-app.use(function(err, req, res, next) {
+app.use(function (err, req, res, next) {
   // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+  res.locals.message = err.message
+  res.locals.error = req.app.get('env') === 'development' ? err : {}
 
   // render the error page
-  res.status(err.status || 500);
-  res.render('error');
-});
+  res.status(err.status || 500)
+  res.render('error')
+})
 
-module.exports = app;
+module.exports = app
