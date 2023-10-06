94d1af8b49ed4383e07f8b1def3e150a16d8fbd2
Alex Just <alexjustdata@gmail.com>
2023-10-06 13:10:23 +0200

    fead: static model Ad

--- nodepop/models/ad.js
+++ nodepop/models/ad.js
@@ -1,28 +1,23 @@
-const mongoose = require('mongoose');
+const mongoose = require('mongoose')
 
 const adSchema = new mongoose.Schema({
-    name: String,
-    option: Boolean,  // true: se vende, false: se busca
-    price: Number,
-    img: String,
-    tags: [String]
-});
+  name: String,
+  option: Boolean, // true: se vende, false: se busca
+  price: Number,
+  img: String,
+  tags: [String]
+})
 
+// método ESTATICO
+adSchema.statics.lista = function (filtro, skip, limit, sort, fields) {
+  const query = Ad.find(filtro) // devuelve un objeto del tipo query que es un thenable
+  query.skip(skip) // http:
+  query.limit(limit) // http:  query.sort(sort); // http:
+  query.select(fields)
 
-
-// método ESTATICO 
-adSchema.statics.lista = function(filtro, skip, limit, sort, fields) { 
-
-    const query = Ad.find(filtro); // devuelve un objeto del tipo query que es un thenable
-    
-    query.skip(skip);   // http://127.0.0.1:3000/api/ads?skip=4  
-    query.limit(limit); // http://127.0.0.1:3000/api/ads?limit=4
-    query.sort(sort);   // http://127.0.0.1:3000/api/ads?sort=price 
-    query.select(fields);
-    
-    return query.exec(); // este si que devuelve la promesa. 
+  return query.exec() // este si que devuelve la promesa.
 }
 
-const Ad = mongoose.model('Ad', adSchema);
+const Ad = mongoose.model('Ad', adSchema)
 
-module.exports = Ad;
+module.exports = Ad
