bb77ecc3255418b5ce7e891ea9621a39db2182f8
Alex Just <alexjustdata@gmail.com>
2023-10-06 13:13:28 +0200

    fead: static Ads

--- nodepop/routes/api/ads.js
+++ nodepop/routes/api/ads.js
@@ -1,7 +1,6 @@
-const express = require('express');
-const router = express.Router();
-const Ad = require('../../models/Ad');
-
+const express = require('express')
+const router = express.Router()
+const Ad = require('../../models/Ad')
 
 // GET Ads API
 // ADS CONTROLLER
@@ -18,91 +17,86 @@ const Ad = require('../../models/Ad');
  */
 
 router.get('/', async (req, res, next) => {
-    try {
-        // Filters
-        // http://127.0.0.1:3000/api/ads?name=Bicicleta
-        const filterByName = req.query.name;
+  try {
+    // Filters
+    // http://127.0.0.1:3000/api/ads?name=Bicicleta
+    const filterByName = req.query.name
 
-        // Pagination
-        // http://127.0.0.1:3000/api/ads?skip=2&limit=7
-        const skip = req.query.skip;
-        const limit = req.query.limit;
+    // Pagination
+    // http://127.0.0.1:3000/api/ads?skip=2&limit=7
+    const skip = req.query.skip
+    const limit = req.query.limit
 
-        // Sorting
-        // http://127.0.0.1:3000/api/ads?sort=-price%20name
-        const sort = req.query.sort;
+    // Sorting
+    // http://127.0.0.1:3000/api/ads?sort=-price%20name
+    const sort = req.query.sort
 
-        // Field Selection
-        // http://localhost:3000/api/ads?fields=name%20-_id%20option
-        const fields = req.query.fields;
+    // Field Selection
+    // http://localhost:3000/api/ads?fields=name%20-_id%20option
+    const fields = req.query.fields
 
-        // Filters
-        // http://127.0.0.1:3000/api/ads?tags=lifestyle,motor
-        const filterByTags = req.query.tags;
+    // Filters
+    // http://127.0.0.1:3000/api/ads?tags=lifestyle,motor
+    const filterByTags = req.query.tags
 
-        // http://127.0.0.1:3000/api/ads?venta=true
-        const filterByType = req.query.venta;
+    // http://127.0.0.1:3000/api/ads?venta=true
+    const filterByType = req.query.venta
 
-        /**
+    /**
          * Para buscar anuncios con un precio entre 10 y 100:
          * http://127.0.0.1:3000/api/ads?price=10-100
          * http://127.0.0.1:3000/api/ads?price=10-
          * http://127.0.0.1:3000/api/ads?price=-100
          * http://127.0.0.1:3000/api/ads?price=100
          */
-        const filterByPrice = req.query.price;
+    const filterByPrice = req.query.price
 
+    const filter = {}
 
-        const filter = {};
-
-
-        if (filterByTags) {
-          filter.tags = { $in: filterByTags.split(',') }; // Si los tags se pasan como una lista separada por comas en la URL
-        }
-
-        if (filterByType !== undefined) {
-          filter.venta = filterByType === 'true'; // Convierte la cadena en un valor booleano
-        }
-        
-        if (filterByName) { // General rule: when creating a filter, add an index to the model [Ads.js]
-          filter.name = filterByName;
-          filter.name = new RegExp('^' + filterByName, 'i'); // Búsqueda insensible a mayúsculas y minúsculas
-        }
-
-        if (filterByPrice) {
-          const priceRange = filterByPrice.split('-');
-
-          if (priceRange.length === 2) {
-            const [minPrice, maxPrice] = priceRange.map(parseFloat);
-            if (!isNaN(minPrice) && !isNaN(maxPrice)) {
-              // Búsqueda de rango de precio
-              filter.price = { $gte: minPrice, $lte: maxPrice };
-            } else if (!isNaN(minPrice)) {
-              // Precio mínimo
-              filter.price = { $gte: minPrice };
-            } else if (!isNaN(maxPrice)) {
-              // Precio máximo
-              filter.price = { $lte: maxPrice };
-            }
-          } else if (priceRange.length === 1) {
-            const singlePrice = parseFloat(priceRange[0]);
-            if (!isNaN(singlePrice)) {
-              // Precio igual
-              filter.price = singlePrice;
-            }
-          }
-        }
-
-    
-        const ads = await Ad.lista(filter, skip, limit, sort, fields);
-
-        res.json({ results: ads })
-
-    } catch (err) {
-        next(err);
-        console.error(err); 
+    if (filterByTags) {
+      filter.tags = { $in: filterByTags.split(',') } // Si los tags se pasan como una lista separada por comas en la URL
     }
-});
 
+    if (filterByType !== undefined) {
+      filter.venta = filterByType === 'true' // Convierte la cadena en un valor booleano
+    }
 
-module.exports = router;
+    if (filterByName) { // General rule: when creating a filter, add an index to the model [Ads.js]
+      filter.name = filterByName
+      filter.name = new RegExp('^' + filterByName, 'i') // Búsqueda insensible a mayúsculas y minúsculas
+    }
+
+    if (filterByPrice) {
+      const priceRange = filterByPrice.split('-')
+
+      if (priceRange.length === 2) {
+        const [minPrice, maxPrice] = priceRange.map(parseFloat)
+        if (!isNaN(minPrice) && !isNaN(maxPrice)) {
+          // Búsqueda de rango de precio
+          filter.price = { $gte: minPrice, $lte: maxPrice }
+        } else if (!isNaN(minPrice)) {
+          // Precio mínimo
+          filter.price = { $gte: minPrice }
+        } else if (!isNaN(maxPrice)) {
+          // Precio máximo
+          filter.price = { $lte: maxPrice }
+        }
+      } else if (priceRange.length === 1) {
+        const singlePrice = parseFloat(priceRange[0])
+        if (!isNaN(singlePrice)) {
+          // Precio igual
+          filter.price = singlePrice
+        }
+      }
+    }
+
+    const ads = await Ad.lista(filter, skip, limit, sort, fields)
+
+    res.json({ results: ads })
+  } catch (err) {
+    next(err)
+    console.error(err)
+  }
+})
+
+module.exports = router
