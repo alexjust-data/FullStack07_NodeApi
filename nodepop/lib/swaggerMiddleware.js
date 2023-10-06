75046ab7d1ae521c34f9a26210f21c442fef9b8d
Alex Just <alexjustdata@gmail.com>
2023-10-06 13:09:27 +0200

    fead: api docs

--- nodepop/lib/swaggerMiddleware.js
+++ nodepop/lib/swaggerMiddleware.js
@@ -2,8 +2,8 @@
 // $ npm install swagger-jsdoc swagger-ui-express
 // to setting up and documenting an API using Swagger, a popular tool for API documentation.
 
-const swaggerJSDoc = require('swagger-jsdoc'); // https://github.com/Surnet/swagger-jsdoc
-const swaggerUI = require('swagger-ui-express'); // https://github.com/scottie1984/swagger-ui-express
+const swaggerJSDoc = require('swagger-jsdoc') // https://github.com/Surnet/swagger-jsdoc
+const swaggerUI = require('swagger-ui-express') // https://github.com/scottie1984/swagger-ui-express
 
 const options = {
   swaggerDefinition: {
@@ -19,9 +19,9 @@ const options = {
    * he copiado en el las lineas que me dió https://editor.swagger.io/
    */
 
-  //apis: ['swagger.yml'] // <-- cargaría el archivo
+  // apis: ['swagger.yml'] //  <-- cargaría el archivo
   apis: ['./routes/**/*.js'] // <-- cargaría esto del archivo *.js (ve a mirarlo):
-                                      /** DOC PARA EL API
+  /** DOC PARA EL API
                                        * @openapi
                                        * /api/agentes:
                                        *  get:
@@ -32,9 +32,9 @@ const options = {
                                        */
 }
 
-const especificacion = swaggerJSDoc(options);
+const especificacion = swaggerJSDoc(options)
 
 module.exports = [
-  swaggerUI.serve, 
+  swaggerUI.serve,
   swaggerUI.setup(especificacion)
-];
+]
